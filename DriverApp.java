7bce046b4794c96c228dfa5fc12437cba062bd65
Bhagamma26 <109431389+Bhagamma26@users.noreply.github.com>
2023-10-03 23:56:16 +0530

    Add files via upload

--- DriverApp.java
+++ DriverApp.java
@@ -1,4 +1,4 @@
-package currency;
+package transactiontargets;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -6,22 +6,48 @@ import java.util.Scanner;
 public class DriverApp {
 
 	public static void main(String[] args) {
+		int nod;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please enter number of currency pf your country");
-		int size = sc.nextInt();
+		System.out.println("Please enter Number of days shop was open");
+		nod = sc.nextInt();
 		
-		int currency[] = new int[size];
-		System.out.println("Please enter the" + size + "currency denominations in any order");
-		for(int i=0; i< currency.length; i++) {
-			currency[i] = sc.nextInt();
+		int trans[] = new int[nod];
+		int i;
+		for ( i = 0; i < trans.length; i++) {
+			System.out.println("Please enter income of Day" + (i+1));
+			trans[i] = sc.nextInt();
 			
 		}
-		System.out.println("Before sorting" +Arrays.toString(currency));
 		
-		MergeSort ms = new MergeSort();
-		ms.sort(currency, 0, size-1);
+		System.out.println(Arrays.toString(trans));
 		
-		System.out.println("After sorting" +Arrays.toString(currency));
-	}
+		System.out.println("Please enter number of targets");
+		int targets = sc.nextInt();
+		
+		int sum;
+		int flag = 0;
+		
+		for(int j=1; j<=targets; j++) {
+			System.out.println("Please enter value for target" +j);
+			int singleTarget = sc.nextInt();
+			flag = 0;  //assumption is target is not achieved
+			sum = 0;
+			for(int k=0; k<=trans.length; k++) {
+				
+				sum = sum + trans[k];
+				if(sum>=singleTarget)
+				{
+					flag = 1;
+					System.out.println("Target achievevd on day" + (k+1));
+					break;
+				}
+			}
+			if(flag==0)
+			{
+				System.out.println("Target not achieved");
+			}
+		}
+
+	} 
 
 }
